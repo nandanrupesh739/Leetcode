acca38c514891a4a9cfb88fe68fe4ac1d9a95e44
Rupesh Nandan <nandanrupesh739@gmail.com>
2026-09-19 22:19:25 +0530

    Time: 6 ms (96.97%) | Memory: 76.1 MB (16.86%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,40 +1,51 @@
-import java.util.concurrent.ThreadLocalRandom;
-
 class Solution {
+
     public int findKthLargest(int[] nums, int k) {
         int target = nums.length - k;
-        int left = 0;
-        int right = nums.length - 1;
+        int left = 0, right = nums.length - 1;
 
         while (left <= right) {
-            int pivotIndex = ThreadLocalRandom.current()
-                    .nextInt(left, right + 1);
 
-            int pivot = nums[pivotIndex];
+            int mid = left + ((right - left) >>> 1);
 
-            // Move pivot to end
-            swap(nums, pivotIndex, right);
+            // Median-of-three pivot
+            int pivot = median(nums[left], nums[mid], nums[right]);
 
-            int p = left;
+            int lt = left;
+            int i = left;
+            int gt = right;
 
-            for (int i = left; i < right; i++) {
+            // 3-way partition
+            while (i <= gt) {
                 if (nums[i] < pivot) {
-                    swap(nums, i, p++);
+                    swap(nums, lt++, i++);
+                } else if (nums[i] > pivot) {
+                    swap(nums, i, gt--);
+                } else {
+                    i++;
                 }
             }
 
-            swap(nums, p, right);
-
-            if (p == target) {
-                return nums[p];
-            } else if (p < target) {
-                left = p + 1;
+            if (target < lt) {
+                right = lt - 1;
+            } else if (target > gt) {
+                left = gt + 1;
             } else {
-                right = p - 1;
+                return pivot;
             }
         }
 
         return -1;
+    }
+
+    private int median(int a, int b, int c) {
+        if (a < b) {
+            if (b < c) return b;
+            return a < c ? c : a;
+        } else {
+            if (a < c) return a;
+            return b < c ? c : b;
+        }
     }
 
     private void swap(int[] nums, int i, int j) {
